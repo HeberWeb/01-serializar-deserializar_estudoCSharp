f2f8a55e157fd329b3be35d524ed8f2be23efba2
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-08 12:39:43 -0300

    04_DeserializarJSON

---
+++ 04_DeserializarJSON/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _00_Biblioteca;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace _04_DeserializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = new StreamReader(@"C:\Users\hpietrafeza\Documents\ProjetosCursos\01-serializar-deserializar\ArquivosTestes\02_SerializarJSON.json");
+            string allLines = stream.ReadToEnd();
+            stream.Close();
+
+            JavaScriptSerializer deserializador = new JavaScriptSerializer();
+            Usuario usuario =  (Usuario)deserializador.Deserialize(allLines, typeof(Usuario));
+
+            Console.WriteLine("Usuário(Nome): {0}, CPF: {1}, Email: {2}", usuario.Nome, usuario.CPF, usuario.Email);
+            Console.ReadKey();
+        }
+    }
+}
