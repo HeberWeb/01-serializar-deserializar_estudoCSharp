225ec72735f60be5965bfc962b98be3fd07189b2
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-08 12:07:56 -0300

    02_DeserializarXML

---
+++ 02_DeserializarXML/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using _00_Biblioteca;
+using System.Xml.Serialization;
+
+namespace _02_DeserializarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader stream = new StreamReader(@"C:\Users\hpietrafeza\Documents\ProjetosCursos\01-serializar-deserializar\ArquivosTestes\01_SerializarXML.xml");
+            XmlSerializer serializador = new XmlSerializer(typeof(Usuario));
+            Usuario usuario = (Usuario)serializador.Deserialize(stream);
+
+            Console.WriteLine("Usuário(Nome): {0}, CPF: {1}, Email: {2}", usuario.Nome, usuario.CPF, usuario.Email);
+            Console.ReadKey();
+        }
+    }
+}
